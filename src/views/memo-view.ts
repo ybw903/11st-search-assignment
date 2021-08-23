959a1e65ca70a2f58e2095bdd2685a4fd43a29ab
ybw903 <conner903@naver.com>
2021-08-23 19:54:37 +0900

    feat: 메모 읽어오기

--- src/views/memo-view.ts
+++ src/views/memo-view.ts
@@ -16,11 +16,43 @@ export default class MemoView {
         this.memos = memos;
     }
 
+    clickEventHandler(e:MouseEvent) {
+        e.preventDefault();
+        const {target} = e;
+        if(!(target instanceof HTMLElement)) return;
+        this.cardOpenHandler(target)
+    }
+
+    cardOpenHandler(target:HTMLElement) {
+        if(target.className!=='memo-card') return;
+
+        const memoContainer = target.closest('.memo');
+        if(!memoContainer) return;
+
+        const memos = [...memoContainer.children];
+        const opendMemo = memos.filter((memo)=>{return memo.classList.contains('open')} )[0];
+        if(opendMemo) {
+            opendMemo.classList.toggle('open');
+        }
+        target.classList.add('open');
+    }
+
+    createMemoCard(memo:Memo,i:number) {
+        return `
+            <div class='memo-card' data-memo-idx=${i}>
+                ${memo.content}
+            </div>
+        `
+    }
+
     render() {
         const content = document.querySelector('.content') as HTMLElement;
         content.innerHTML = `
-            <div>
-                Memo
+            <div class='memo'>
+                ${this.memos.reduce(
+                    (a,b,i) => a+ this.createMemoCard(b,i),
+                    ''
+                )}
             </div>
         `
     }
