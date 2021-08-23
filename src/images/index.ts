5f141ec26f9431fc701a57583daf46d73716e2b5
ybw903 <conner903@naver.com>
2021-08-23 22:37:22 +0900

    feat: 사진앱

---
+++ src/images/index.ts
@@ -0,0 +1,14 @@
+import apple from './apple.jpg';
+import banana from './banana.jpeg';
+import grape from './grape.jpg';
+import korean_mellon from './korean_melon.jpg';
+import mango from './mango.jpg';
+import mellon from './melon.jpg';
+import peach from './peach.jpg';
+import plum from './plum.jpg';
+import strawberry from './strawberry.jpg';
+import water_melon from './water_melon.jpg';
+
+const imgs = [apple, banana, grape, korean_mellon, mango, mellon, peach, plum, strawberry, water_melon];
+
+export default imgs;
--- src/views/photo-view.ts
+++ src/views/photo-view.ts
@@ -1,16 +1,61 @@
 import cem from '../custom-event';
+import imgs from '../images/index';
 export default class PhotoView {
     constructor() {
-        cem.subscribe('storeupdated',((e: CustomEvent) => {
+        cem.subscribe('storeupdated',((e:CustomEvent)=> {
             if(e.detail.path !== '/photo') return;
             this.render();
-        })as EventListenerOrEventListenerObject)
+            const photo =  document.querySelector('.photo') as HTMLElement;
+            photo.addEventListener('click', this.clickEventHandler.bind(this));
+        }) as EventListenerOrEventListenerObject);
     }
+
+    makeSlider() {
+        return `
+            <div class='slider'>
+                ${imgs.reduce((a,b) => {  
+                    console.log(b);
+                    return a + `
+                    <img class="slider-img src="${b}" alt="${b}">
+                `},'')}
+            </div>
+        `
+    }
+
+    clickEventHandler(e:MouseEvent) {
+        e.preventDefault();
+        const {target} = e;
+        if(!(target instanceof HTMLElement))
+            return;
+        this.imgSelectHandler(target)
+    }
+
+    imgSelectHandler (target: HTMLElement) {
+
+            const sliderImg = target.closest('.slider-img');
+            if(!sliderImg) return;
+
+            const curSelectedImg = document.querySelector('.selected');
+
+            const imgWrapper = document.querySelector('.img-wrapper') as HTMLElement;
+            const img = imgWrapper.firstElementChild;
+
+            curSelectedImg?.classList.remove('selected');
+
+            sliderImg.classList.add('selected');
+            img?.setAttribute('src', sliderImg.getAttribute('src')??''); 
+    }
+
+
     render() {
         const content = document.querySelector('.content') as HTMLElement;
-        content.innerHTML = `
-            <div>
-                Photo
+        content.innerHTML =
+        `
+            <div class = 'photo'>
+                ${this.makeSlider()}
+                <div class='img-wrapper'>
+                    <img  src="./img/1.jpg"/>
+                </div>
             </div>
         `
     }
